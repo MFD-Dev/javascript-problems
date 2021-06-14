042169b610d2e4463acb418b0f5cd258901565bf
Michael <Michaelflowersdev@gmail.com>
2021-06-14 16:10:04 -0500

    added write a javascript function that accepts a string as a parameter and find the

--- functions-problems/app.js
+++ functions-problems/app.js
@@ -10,16 +10,44 @@ longest word within the parameter.
 7.  return that result.
 */
 
-function longestWord(str) {
-   let wordLength = str.split(', '); // split string with comma space
-   let maxLength = 0
-   
-   for (let i = 0; i < wordLength.length; i++) {
-        if (wordLength[i] > maxLength) {
-            maxLength = words[i].length;
-        }
-    }
-    return maxLength;
-}
 
-longestWord('hello, world, newWorld')
+  function findLongestWordLength(str) {  // create a function to hold the longest word.  Pass a string through the function.
+    let words = str.split(', '); // use the .split() method to separate via  a comma.
+    let maxLength = 0;  //create a variable to start at 0. 
+  
+    for (let i = 0; i < words.length; i++) {  // create a for loop to loop through the words variable and use the .length() method 
+      //to measure and iterate one at a time.
+      if (words[i].length > maxLength) {  //compare words in array that are greater than 0.
+        maxLength = words[i].length;  // creates a variable that loops through the array and uses the .length() method to find the longest word.
+      }
+    }
+    return maxLength;  // returns the longest word
+  }
+  
+
+console.log(findLongestWordLength('hello', 'worlds', 'hello world')); 
+
+
+// other methods: 1. find() 2. sort() 3. map()
+
+
+
+
+
+
+// let wordList  = ('hello, world, newWorld')
+
+// function longestWord(str) {
+//     let wordList = str.split(', '); // use the split method to separate string based on comma space
+//    let maxLength = 0;  // maximum base length
+   
+//    for (let i = 0; i < wordList.length; i++) {  // loop through array based on lenght method
+//     // iterating one at a time. 
+//         if (wordList[i] > maxLength) { //looping through array indexes--looking to see which one is 
+//             // greater than maxLength
+//             maxLength = words[i].length;  // when the maxlength word = the longest word in the array
+//         }
+//     }
+//     return maxLength; // returns longest word
+// }
+
