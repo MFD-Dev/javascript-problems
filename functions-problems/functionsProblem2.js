08b713811747700fcd1c2d90b6b5e0861b068bc1
Michael <Michaelflowersdev@gmail.com>
2021-06-14 16:48:49 -0500

    initial pseudo code

---
+++ functions-problems/functionsProblem2.js
@@ -0,0 +1,13 @@
+/*
+Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+Hint: use the "spilt", the Array#includes method and, a "for" loop or "forEach" loop.
+1.  create  a variable that represents all vowels.
+2.  create a base line count variable that represents 0.
+3.  create a function that allows me to pass in a string parmeter as a argument. 
+Within the function:
+4.  create a For Loop that loops through the array and iterates one at a time.  
+5.  go through the string and search for vowels.
+6.  create a split()method for the words in the array.
+7.  return the total number of matches found.
+ 
+*/
