d9d3ce5dc3eff3cbf5d1883491037ea3e5bb67b9
Michael <Michaelflowersdev@gmail.com>
2021-07-03 10:52:26 -0500

    updated and added a function w/no for loop

--- functions-problems/functionsProblem4.js
+++ functions-problems/functionsProblem4.js
@@ -11,24 +11,36 @@
 
 */
 
-// create a function that accepts a string as a argument
-function checkIfPalindrome(str) {
-  // create a variable to find string length
-  const len = string.length;
-  
-  // loop through half of the string 
-  for (let i = 0; i < len / 2; i++) {
-    // check if half of the string and last half are the same
-    if (string[i] !== string[len - 1 -i]) {
-      return 'It is not a palindrome'
-    }
-  }
-  return 'It is a palindrome'
+function checkPalindrome(str){
+  let reversed = str.split("").reverse().join("")
+  return str === reversed
 }
-// input string
-// const string  = 'Hello Mr. Palindrome.'
-   const string = 'racecar'
-// call the function 
-const result = checkIfPalindrome(string)
+let str1 = "hellopalindrome"
+let str2 = "racecar"
 
-console.log(result)
+
+checkPalindrome(str1);
+checkPalindrome(str2);
+
+
+// // create a function that accepts a string as a argument
+// function checkIfPalindrome(str) {
+//   // create a variable to find string length
+//   const len = string.length;
+  
+//   // loop through half of the string 
+//   for (let i = 0; i < len / 2; i++) {
+//     // check if half of the string and last half are the same
+//     if (string[i] !== string[len - 1 -i]) {
+//       return 'It is not a palindrome'
+//     }
+//   }
+//   return 'It is a palindrome'
+// }
+// // input string
+// // const string  = 'Hello Mr. Palindrome.'
+//    const string = 'racecar'
+// // call the function 
+// const result = checkIfPalindrome(string)
+
+// console.log(result)
